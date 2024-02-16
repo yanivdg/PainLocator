795aae4f820d227e36246d6a0efc10bb14394954
yanivdg <134487944+yanivdg@users.noreply.github.com>
2024-02-16 23:45:36 +0200

    Update solar_system.js

--- solar_system.js
+++ solar_system.js
@@ -25,7 +25,7 @@ scene.add(sun);
 var planetGeometry = new THREE.SphereGeometry(1, 32, 32);
 // Load the texture
 var textureLoader = new THREE.TextureLoader();
-var planetTexture = textureLoader.load('https://th.bing.com/th/id/OIG1.7SfIi.qd2tVabSHH6UTB?w=1024&h=1024&rs=1&pid=ImgDetMain'); // replace with your texture path
+var planetTexture = textureLoader.load('https://t4.ftcdn.net/jpg/02/24/14/81/360_F_224148194_xrGaLP6RZbCL7B3vOMYYr2dVrcg95RFt.jpg'); // replace with your texture path
 var planetMaterial = new THREE.MeshBasicMaterial({map: planetTexture});
 var planet = new THREE.Mesh(planetGeometry, planetMaterial);
 planet.position.x = 10;
