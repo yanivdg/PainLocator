b2811c7518f94e10c0439a9065767397db2eb396
yanivdg <134487944+yanivdg@users.noreply.github.com>
2024-02-17 13:59:14 +0200

    Update solar_system.js

--- solar_system.js
+++ solar_system.js
@@ -81,7 +81,7 @@ var animate = function () {
             if (object instanceof THREE.Mesh && object !== sun) 
             {
               // Increase the elapsed time value to speed up the animation
-              var time = clock.getElapsedTime() * 100; // Adjust the factor as needed
+              var time = clock.getElapsedTime() * 50; // Adjust the factor as needed
               object.position.x = sun.position.x + object.userData.distance * Math.cos(time * object.userData.speed);
               object.position.z = sun.position.z + object.userData.distance * Math.sin(time * object.userData.speed);
             }
